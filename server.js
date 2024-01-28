88041b04fb3a0d513083d1d986c6d55fb786e94d
rkeenan <ross.keenan@protonmail.comm>
2024-01-28 11:55:46 -0600

    Return a list of users.

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+// Fetch users
+app.get("/users", (req, res) => {
+  const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Doe" },
+    { id: 3, name: "Tyrone Biggums" },
+  ];
+
+  res.send(`<h1 class="text-2xl font-bold my-4">Users</h1>
+      <ul>
+        ${users.map((user) => `<li>${user.name}</li>`).join("")}
+      </ul>`);
+});
+
 // Start the server
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
