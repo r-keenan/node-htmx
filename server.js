fb20019d57e1a5cd1a4a59e5b09644199d644214
rkeenan <ross.keenan@protonmail.comm>
2024-01-28 12:08:58 -0600

    Added a value to pass along with the request for limit.

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ app.get("/users", async (req, res) => {
   //];
 
   setTimeout(async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const limit = +req.query.limit || 10;
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
     const users = await response.json();
 
     res.send(`<h1 class="text-2xl font-bold my-4">Users</h1>
