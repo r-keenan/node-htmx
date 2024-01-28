10496872366304d784c96691dfd6d3ab381c1f72
rkeenan <ross.keenan@protonmail.comm>
2024-01-28 12:05:01 -0600

    Added setTimeout to simulate a slower call and to show that the actual loading spinner is working.

--- server.js
+++ server.js
@@ -17,13 +17,15 @@ app.get("/users", async (req, res) => {
   //  { id: 3, name: "Tyrone Biggums" },
   //];
 
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  setTimeout(async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
 
-  res.send(`<h1 class="text-2xl font-bold my-4">Users</h1>
+    res.send(`<h1 class="text-2xl font-bold my-4">Users</h1>
       <ul>
         ${users.map((user) => `<li>${user.name}</li>`).join("")}
       </ul>`);
+  }, 2000);
 });
 
 // Start the server
