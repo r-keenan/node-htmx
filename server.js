2b13eecbc462e6b85a2b48b0dbf31d7a26db6031
rkeenan <ross.keenan@protonmail.comm>
2024-01-28 12:00:03 -0600

    Switch from hardcoded users array to api call for fake users.

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Fetch users
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Doe" },
-    { id: 3, name: "Tyrone Biggums" },
-  ];
+app.get("/users", async (req, res) => {
+  //const users = [
+  //  { id: 1, name: "John Doe" },
+  //  { id: 2, name: "Jane Doe" },
+  //  { id: 3, name: "Tyrone Biggums" },
+  //];
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
 
   res.send(`<h1 class="text-2xl font-bold my-4">Users</h1>
       <ul>
